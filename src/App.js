9af8219f6e4eee478081de13349dc04bcf6e439a
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-03 11:03:24 +0100

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <div className="container">
+      <h1>Weather App</h1>
       <footer>
       This project is created by Tracey Lintott and is open-sourced on {" "}
       <a href="https://github.com/TLintott/weather-app-react" target='blank'>Github</a>
     </footer>
+    </div>
     </div>
   );
 }
