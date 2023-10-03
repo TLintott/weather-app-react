99bcf83da0290cfe279b6cbdd07199bf59af8319
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-03 10:31:13 +0100

    added the github link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ export default function App() {
   return (
     <div className="App">
       Weather App
+      <footer>
+      This project is created by Tracey Lintott and is open-sourced on {" "}
+      <a href="https://github.com/TLintott/weather-app-react" target='blank'>Github</a>
+    </footer>
     </div>
   );
 }
