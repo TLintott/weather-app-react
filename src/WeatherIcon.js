6e172e9d3f8451b47b35f7d81d535143b30fcb01
TLintott <traceyjlintott@hotmail.co.uk>
2023-10-10 14:27:00 +0100

    colours modified

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ const codeMapping = {
     return (
 <ReactAnimatedWeather
     icon={codeMapping[props.code]}
-    color="#314ba1"
+    color="#111d42"
     size={props.size}
     animate={true}
   />
